1fa33ee0b560b3321604287652c34e344d61d93a
Spencer Austman <spencerlgaustman@gmail.com>
2017-11-30 01:57:17 -0800

    Added command to completely flush a channel's messages

--- src/akira.py
+++ src/akira.py
@@ -3,17 +3,20 @@ from discord.ext import commands
 import random
 import time
 
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='?', description="asdf")
 
 @bot.event
 async def on_ready():
     print('Logged in as ' + bot.user.name + ' ' + bot.user.id)
 
-#  @bot.event
-#  async def on_message(message):
-#      await bot.send_message(message.channel, 'Hello World!')
-
-
+"""Deletes every message in a given channel"""
+@bot.command(pass_context = True)
+async def flushChannel(ctxt):
+   messages = []
+   async for message in bot.logs_from(ctxt.message.channel_mentions[0], limit = 1000000):
+     messages.append(message)
+   while len(messages) > 0:
+     await bot.delete_messages(messages[0:99])
 
 #@bot.command(pass_context=True)
 #async def sendInvite(ctxt):
